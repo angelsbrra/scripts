15645ec9aad4956665c7bd29814107657f1984a4
angelsbrra <84992568+angelsbrra@users.noreply.github.com>
2025-03-22 10:27:06 +0100

    Create oldgacha.lua

---
+++ oldgacha.lua
@@ -0,0 +1,18 @@
+roll = true
+
+
+local items = {
+    -- "SpiritualBeasts",
+    -- "TrialRoll",
+    "EssenceStones",
+	"Bonkais"
+}
+
+local doGacha = game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Remotes"):WaitForChild("doGacha")
+
+while roll do
+    for _, item in ipairs(items) do
+        doGacha:InvokeServer("Roll", item)
+    end
+    task.wait() -- Prevents excessive CPU usage
+end
