01858854ed272bf847302f36e0d3a785edcdb719
angelsbrra <84992568+angelsbrra@users.noreply.github.com>
2025-03-31 21:02:51 +0200

    Update timejoin.lua

--- timejoin.lua
+++ timejoin.lua
@@ -1,4 +1,3 @@
--- Function to run code 1 at specified times
 local function runCode1()
     local args = {
         "Join",
@@ -8,7 +7,6 @@ local function runCode1()
     print("Running Code 1!")
 end
 
--- Function to run code 2 at specified times
 local function runCode2()
     local args = {
         "Join",
@@ -27,41 +25,30 @@ local function runCode3()
     print("Running Code 3!")
 end
 
--- This function checks if it's time to run specific code
 local function checkTime()
-    -- Define the specific minutes and the corresponding code functions
-    local code1Times = {"19", "49"}  -- Minutes for Code 1
-    local code2Times = {"29", "59"}  -- Minutes for Code 2
-    local code3Times = {"39", "09"}  -- Minutes for Code 2
+    local code1Times = {"19", "49"} 
+    local code2Times = {"29", "59"}
+    local code3Times = {"39", "09"}
     
     while true do
-        -- Get the current time in "hour:minute" format
         local currentTime = os.date("%H:%M")
         local hour = os.date("%H")
         local minute = os.date("%M")
         
-        -- Check if it's time to run Code 1
         if table.find(code1Times, minute) then
-            print("Running Code 1 at " .. hour .. ":" .. minute)
             runCode1()
         end
         
-        -- Check if it's time to run Code 2
         if table.find(code2Times, minute) then
-            print("Running Code 2 at " .. hour .. ":" .. minute)
             runCode2()
         end
 
         if table.find(code3Times, minute) then
-            print("Running Code 3 at " .. hour .. ":" .. minute)
             runCode3()
         end
 
-
-        -- Wait for 60 seconds before checking again
         wait(10)
     end
 end
 
--- Start the time check loop
 checkTime()
