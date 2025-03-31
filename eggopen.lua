bac9dc1d331321363faaaf65ea8de0b0985975a5
angelsbrra <84992568+angelsbrra@users.noreply.github.com>
2025-03-31 20:59:08 +0200

    Delete eggopen.lua

--- eggopen.lua
+++
@@ -1,8 +0,0 @@
-while true do 
-local args = {
-	"500KWorld",
-	"Multi"
-}
-game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Remotes"):WaitForChild("openEssence"):InvokeServer(unpack(args))
-wait(4)
-end
