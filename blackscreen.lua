cd0841c3debbb46214bd431b752933ab82a47d9b
angelsbrra <84992568+angelsbrra@users.noreply.github.com>
2025-03-31 20:59:50 +0200

    Update blackscreen.lua

--- blackscreen.lua
+++ blackscreen.lua
@@ -1,12 +1,12 @@
 local player = game.Players.LocalPlayer
 local gui = Instance.new("ScreenGui", player:WaitForChild("PlayerGui"))
-gui.IgnoreGuiInset = true -- Ensures it covers the entire screen without UI gaps
+gui.IgnoreGuiInset = true 
 
 local frame = Instance.new("Frame", gui)
 
-frame.Size = UDim2.new(1, 0, 1, 0) -- Full-screen
-frame.BackgroundColor3 = Color3.new(0, 0, 0) -- Black color
-frame.Visible = true -- Hidden by default
+frame.Size = UDim2.new(1, 0, 1, 0)
+frame.BackgroundColor3 = Color3.new(0, 0, 0)
+frame.Visible = true
 
 -- Toggle when player presses "B"
 game:GetService("UserInputService").InputBegan:Connect(function(input, gameProcessed)
